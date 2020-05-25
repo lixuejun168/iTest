ab21da8d82f14d3f44da4eb376812989f2593dfb
lxj1 <xuejunhxf@163.com>
2020-05-25 13:44:21 +0800

    v41

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+
+    tools {
+        maven 'mvn-3.3.3'
+    }
+
+    stages {
+        stage('Build'){
+            steps {
+                git([url: 'git@github.com:lixuejun168/iTest.git', branch: 'master'])
+                sh 'printenv'
+                sh "pwd"
+                sh "mvn clean install test"
+            }
+
+            post {
+                always {
+                    junit testResults: "**/target/surefire-reports/*.xml"
+                }
+            }
+        }
+        
+    }
+
+    
+}
